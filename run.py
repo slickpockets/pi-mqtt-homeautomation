3e37baae7a96dc543e3f6831d387c81243a3144f
slickpockets <slickpockets@centraldogmapocket.lan>
2021-08-22 02:36:06 +0800

    c

--- run.py
+++ run.py
@@ -7,5 +7,5 @@ import logging
 if __name__ == "__main__":
     logging.basicConfig(filename='/var/log/python/flask.log',level=logging.DEBUG)
     app = create_app()
-    app.run(app, '0.0.0.0' port=5000, use_reloader=True, debug=True)
+    app.run(app, '0.0.0.0', 5000, use_reloader=True, debug=True)
     logging.info("must turn off autoreloader in production")
